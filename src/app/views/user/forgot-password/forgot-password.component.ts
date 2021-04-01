e037c01eeb57ffa1e1a2d2dffe998ca71638f6d6
Spand0x <gexna97@gmail.com>
2021-04-01 23:57:03 +0300

    Config for heroku deploy

--- src/app/views/user/forgot-password/forgot-password.component.ts
+++ src/app/views/user/forgot-password/forgot-password.component.ts
@@ -20,25 +20,25 @@ export class ForgotPasswordComponent implements OnInit {
   }
 
   onSubmit() {
-    if (!this.passwordForm.valid || this.buttonDisabled) {
-      return;
-    }
-    this.buttonDisabled = true;
-    this.buttonState = 'show-spinner';
-
-    this.authService.sendPasswordEmail(this.passwordForm.value.email).subscribe((answer) => {
-    }, (error) => {
-      // this.notifications.create('Error', error.message, NotificationType.Bare, { theClass: 'outline primary', timeOut: 6000, showProgressBar: false });
-      this.buttonDisabled = false;
-      this.buttonState = '';
-    }, () => {
-      // this.notifications.create('Done', 'Password reset email is sent, you will be redirected to Reset Password page!', NotificationType.Bare, { theClass: 'outline primary', timeOut: 6000, showProgressBar: true });
-      this.buttonDisabled = false;
-      this.buttonState = '';
-      setTimeout(() => {
-        this.router.navigate(['user/reset-password']);
-      }, 6000);
-    });
+  //   if (!this.passwordForm.valid || this.buttonDisabled) {
+  //     return;
+  //   }
+  //   this.buttonDisabled = true;
+  //   this.buttonState = 'show-spinner';
+  //
+  //   this.authService.sendPasswordEmail(this.passwordForm.value.email).subscribe((answer) => {
+  //   }, (error) => {
+  //     // this.notifications.create('Error', error.message, NotificationType.Bare, { theClass: 'outline primary', timeOut: 6000, showProgressBar: false });
+  //     this.buttonDisabled = false;
+  //     this.buttonState = '';
+  //   }, () => {
+  //     // this.notifications.create('Done', 'Password reset email is sent, you will be redirected to Reset Password page!', NotificationType.Bare, { theClass: 'outline primary', timeOut: 6000, showProgressBar: true });
+  //     this.buttonDisabled = false;
+  //     this.buttonState = '';
+  //     setTimeout(() => {
+  //       this.router.navigate(['user/reset-password']);
+  //     }, 6000);
+  //   });
   }
 
 }
